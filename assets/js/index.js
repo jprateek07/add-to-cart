c7277316f0d5b91c9f15a596d833fde619850c24
prateek jain <jprateek07@gmail.com>
2021-02-05 11:11:02 +0530

    added button enable and disable functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,8 @@ function newElement(e) {
         "<td>" + e.parentNode.parentNode.childNodes[3].innerHTML+ "</td>" +
         "<td>" +e.parentNode.parentNode.childNodes[5].innerHTML + "</td>" +
         "<td><div class='d-flex'><button onclick='decrement(this)' class='btn-sm btn-danger'><span class='btn-inner--icon'><i class='fas fa-minus'></i></span></button><span class='btn-inner--text'><input type='number' name='qty' class='text-center' id='qty' min='1' value='1'></span><button class='btn-sm btn-danger' onclick='increment(this)'><span class='btn-inner--icon'><i class='fa fa-plus' aria-hidden='true'></i></span></button></div></td>" +
-        "<td><button type='button' class='btn-sm btn-danger rounded-pill'>Add</button></td>"+
+        "<td><button type='button' class='btn-sm btn-success rounded-pill'>Add</button></td>"+
+        "<td><button type='button' onclick='deleteRow(this)' class='btn-sm btn-danger rounded-pill'>Remove</button></td>"+
         "</tr>");
 }
 function increment(curr)
@@ -14,8 +15,17 @@ function decrement(curr)
 {
     curr.parentNode.childNodes[1].childNodes[0].stepDown();
 }
-// function deleteRow(r) {
-    // call this function by this deleteRow(this)
-//     var i = r.parentNode.parentNode.rowIndex;
-//     document.getElementById("myTable").deleteRow(i);
-//   }
+function deleteRow(r) {
+    var index = r.parentNode.parentNode.rowIndex;
+    var wishlistItem=r.parentNode.parentNode.childNodes[0].innerHTML
+    var rowLength=document.getElementById("product-table").rows.length
+    for(i=1;i<rowLength;i++)
+    {
+        var productItem = document.getElementById("product-table").rows[i].cells[1].innerHTML
+        if(productItem==wishlistItem)
+        {
+            document.getElementById("product-table").rows[i].cells[3].childNodes[0].disabled=false
+        }
+    }
+    document.getElementById("wishlist-table").deleteRow(index);
+  }

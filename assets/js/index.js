e7a33fa058546f495f3797f7e06e167fef6fd040
prateek jain <jprateek07@gmail.com>
2021-02-05 13:38:00 +0530

    added cart modal

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,9 +4,19 @@ function newElement(e) {
         "<td>" +e.parentNode.parentNode.childNodes[5].innerHTML + "</td>" +
         "<td><div class='d-flex'><button onclick='decrement(this)' class='btn-sm btn-danger'><span class='btn-inner--icon'><i class='fas fa-minus'></i></span></button><span class='btn-inner--text'><input type='number' name='qty' class='text-center' id='qty' min='1' value='1'></span><button class='btn-sm btn-danger' onclick='increment(this)'><span class='btn-inner--icon'><i class='fa fa-plus' aria-hidden='true'></i></span></button></div></td>" +
         "<td>"+'Rs.'+e.parentNode.parentNode.childNodes[5].innerHTML + "</td>"+
-        "<td><button type='button' class='btn-sm btn-success rounded-pill'>Add</button></td>"+
+        "<td><button type='button' class='btn-sm btn-success rounded-pill' onclick='addToCart(this);this.disabled=true;'>Add</button></td>"+
         "<td><button type='button' onclick='deleteRow(this)' class='btn-sm btn-danger rounded-pill'>Remove</button></td>"+
         "</tr>");        
+}
+function addToCart(e)
+{
+    var qty=e.parentNode.parentNode.childNodes[2].childNodes[0].childNodes[1].childNodes[0].value
+    // var qty=e.parentNode.parentNode.childNodes[1].innerHTML
+    var amt=e.parentNode.parentNode.childNodes[3].innerHTML
+    $("#cart-items").append("<tr>" +
+    "<td>" + e.parentNode.parentNode.childNodes[0].innerHTML+ "</td>" +
+    "<td>" +amt + "</td>" +
+    "</tr>"); 
 }
 function increment(curr)
 {

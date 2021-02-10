9236fc0991d9eb03ce8c0a14404e061a9635aa27
prateek jain <jprateek07@gmail.com>
2021-02-10 11:07:36 +0530

    cart section updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,42 @@
-function addToCart(e)
+var cartData=[]
+var data=[]
+$.ajax({
+    url:'assets/data.json',
+    dataType:'json',
+    success: function (data){
+        loadData(data)
+    },
+    error: function (err) {
+                alert(err);
+            }
+})
+function loadData(jsonData)
+{      
+    data=jsonData
+    $.each(jsonData , function(index, item) {
+        if(item.category=="vegetable")
+        {
+        var vegetableSection=$('#vegetable')  
+        vegetableSection.append("<div class='col-md-3' id='"+item.id+"'><div class='card my-4'><div class='vegetable-img'><img src='"+item.itemSrc+"' class='card-img-top vegetables' alt='...'></div><div class='card-body'><h5 class='card-title item-name'>"+item.itemName+"</h5><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><p class='text-dark'>1kg</p><div class='d-flex justify-content-between align-items-center'><p class='card-text font-weight-bold'>&#x20B9<span class='item-price'>"+item.itemPrice+"</span></p><button type='button' class='btn btn-outline-danger rounded-pill add-to-cart' id='image-"+item.id+"' onclick='addToCart("+item.id+");this.disabled=true'>Add to cart</button></div></div></div></div>");
+        }
+        else if(item.category=="fruit")
+        {
+            var fruitSection=$('#fruit')
+            fruitSection.append("<div class='col-md-3' id='"+item.id+"'><div class='card my-4'><div class='vegetable-img'><img src='"+item.itemSrc+"' class='card-img-top vegetables' alt='...'></div><div class='card-body'><h5 class='card-title item-name'>"+item.itemName+"</h5><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star checked'></span><span class='fa fa-star'></span><span class='fa fa-star'></span><p class='text-dark'>1kg</p><div class='d-flex justify-content-between align-items-center'><p class='card-text font-weight-bold'>&#x20B9<span class='item-price'>"+item.itemPrice+"</span></p><button type='button' class='btn btn-outline-danger rounded-pill add-to-cart' onclick='addToCart("+item.id+");this.disabled=true'>Add to cart</button></div></div></div></div>");
+        }
+    })
+}
+function addToCart(id)
 {
-    e.parentNode.parentNode.parentNode.classList.add('cart-animation')
-    var itemName=e.parentNode.parentNode.parentNode.querySelector(".item-name").innerHTML
-    var imge=e.parentNode.parentNode.parentNode.querySelector(".vegetable-img").childNodes[1]
-    var amt=e.parentNode.querySelector(".item-price").innerHTML
-    var qty=1
-    var Imgcontainer=document.createElement("div")
-    var cartImg =document.createElement("img")
-    cartImg.src=imge.src
-    cartImg.className="cart-item-image"
-    Imgcontainer.appendChild(cartImg)
+    const currentObject = Object.values(data).find(item => item.id === id);
+    cartData.push(currentObject)
     $("#cart-items").append("<tr>" +
-    "<td>" +Imgcontainer.innerHTML+"<p class='text-center cart-item-name'>"+itemName+"</p></td>" +
-    "<td><div class='d-flex align-items-center'><div class='dec-btn' onclick='decrement(this)'><i class='fas fa-minus text-white'></i></div><span class='btn-inner--text'><input type='number' name='qty' class='text-center' id='qty' min='1' value='1'></span><div class='dec-btn' onclick='increment(this)'><i class='fa fa-plus text-white' aria-hidden='true'></i></div></div></td>" +
-    "<td id='totalamt'> Rs. "+amt+"</td>"+
-    "<td><button type='button' onclick='deleteRow(this)' class='btn-sm btn-danger rounded-pill'><i class='fa fa-trash' aria-hidden='true'></i></button></td>"+
+    "<td><img src='"+currentObject.itemSrc+"' class='cart-item-image'><p class='text-center cart-item-name'>"+currentObject.itemName+"</p></td>" +
+    "<td><div class='d-flex align-items-center'><div class='dec-btn' onclick='decrement(this,"+currentObject.id+")'><i class='fas fa-minus text-white'></i></div><span class='btn-inner--text'><input type='number' name='qty' class='text-center' id='qty' min='1' value='1'></span><div class='dec-btn' onclick='increment(this,"+currentObject.id+")'><i class='fa fa-plus text-white' aria-hidden='true'></i></div></div></td>" +
+    "<td id='totalamt'> Rs. "+currentObject.itemPrice+"</td>"+
+    "<td><button type='button' onclick='deleteRow(this,"+currentObject.id+")' class='btn-sm btn-danger rounded-pill'><i class='fa fa-trash' aria-hidden='true'></i></button></td>"+
     "</tr>");
-    var cartRows=e.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("#cart-table").rows.length
-    e.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector("#lblCartCount").innerHTML=cartRows-1 
+    document.getElementById("lblCartCount").innerHTML=document.getElementById("cart-table").rows.length-1
     updateTotalAmount()  
 }
 function  updateTotalAmount()
@@ -33,39 +52,46 @@ function  updateTotalAmount()
     document.getElementById("final-amt").innerHTML=total    
 
 }
-function increment(curr)
+function increment(curr,id)
 {
+    const currentObject = Object.values(cartData).find(item => item.id === id);
     curr.parentNode.childNodes[1].childNodes[0].stepUp();
-    var itemPrice=curr.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector(".item-price").innerHTML
+    var itemPrice=currentObject.itemPrice
     var qty=curr.parentNode.querySelector("#qty").value
     var totalAmount=itemPrice*qty
     curr.parentNode.parentNode.parentNode.childNodes[2].innerHTML="Rs. "+totalAmount
     updateTotalAmount()
 }
-function decrement(curr)
+function decrement(curr,id)
 {
+    const currentObject = Object.values(cartData).find(item => item.id === id);
     curr.parentNode.childNodes[1].childNodes[0].stepDown();
-    var itemPrice=curr.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector(".item-price").innerHTML
+    var itemPrice=currentObject.itemPrice
     var qty=curr.parentNode.querySelector("#qty").value
     var totalAmount=itemPrice*qty
     curr.parentNode.parentNode.parentNode.childNodes[2].innerHTML="Rs. "+totalAmount
     updateTotalAmount()
 }
-function deleteRow(r) {
-    var cartItem=r.parentNode.parentNode.querySelector(".cart-item-name").innerHTML
-    var index = r.parentNode.parentNode.rowIndex;
-    const elements = document.querySelectorAll('.item-name');
-    Array.from(elements).forEach((element, index) => {
-        var itemName=element.innerHTML
-        if(itemName==cartItem)
-        {
-            element.parentNode.querySelector(".add-to-cart").disabled=false
-        }
-      });
-    var bodypart=r.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode
-    r.parentNode.parentNode.parentNode.parentNode.deleteRow(index)
-    var cartRows=bodypart.querySelector("#cart-table").rows.length
-    bodypart.querySelector("#lblCartCount").innerHTML=cartRows-1
+function UpdateCart()
+{
+    $("#cart-items").empty()
+    for( index in cartData)
+    {
+        console.log("inside")
+        $("#cart-items").append("<tr>" +
+        "<td><img src='"+cartData[index].itemSrc+"' class='cart-item-image'><p class='text-center cart-item-name'>"+cartData[index].itemName+"</p></td>" +
+        "<td><div class='d-flex align-items-center'><div class='dec-btn' onclick='decrement(this,"+cartData[index].id+")'><i class='fas fa-minus text-white'></i></div><span class='btn-inner--text'><input type='number' name='qty' class='text-center' id='qty' min='1' value='1'></span><div class='dec-btn' onclick='increment(this,"+cartData[index].id+")'><i class='fa fa-plus text-white' aria-hidden='true'></i></div></div></td>" +
+        "<td id='totalamt'> Rs. "+cartData[index].itemPrice+"</td>"+
+        "<td><button type='button' onclick='deleteRow(this,"+cartData[index].id+")' class='btn-sm btn-danger rounded-pill'><i class='fa fa-trash' aria-hidden='true'></i></button></td>"+
+        "</tr>");
+    }
+    document.getElementById("lblCartCount").innerHTML=document.getElementById("cart-table").rows.length-1
+}
+function deleteRow(r,id) {
+    document.getElementById("image-"+id).disabled=false
+    const currentObject = Object.values(cartData).find(item => item.id === id);
+    cartData=cartData.filter(function(item){return item.id!=id})
+    UpdateCart()
     updateTotalAmount()
   }
   
